3dc52f4b2aba179fddb2649d1ec4b275d762eca7
Max Hackinger <mahaxi@gmail.com>
2019-08-03 23:04:55 -0400

    did some refactoring: pulled out the call to git from main in to seperate function and added clap for commandline arguments

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,67 @@
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
+use std::borrow::Borrow;
 use std::env;
-use std::fs;
-use std::path::Path;
 use std::ffi::OsStr;
+use std::fs;
 use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::path::Path;
 use std::process::{Command, Stdio};
 use std::str;
-use std::borrow::Borrow;
 
 //The mvp idea is to simple create a programm that goes in to each folder
 // and calls git pull to update the project to the latest state.
 
 fn main() {
-    let file_path = "./";
+    let matches = App::new("The git updater")
+        .version("0.1").author("Max Hackinger")
+        .about("this is a simple programm that updates all the git repositories laying around on your maschin that you want to update")
+        .arg(Arg::with_name("directory")
+            .short("d")
+            .long("directory")
+            .value_name("file_path")
+            .help("sets the directory path from which the git repos should be updated")
+            .required(true))
+        .get_matches();
+
+    let file_path = matches.value_of("directory").unwrap();
+    println!(
+        "git updater will start updating all git repos in {}",
+        file_path
+    );
 
     let paths = fs::read_dir(file_path).unwrap();
-    println!("git updater will start updating all git repos in {}", file_path);
 
     for path in paths {
+        let path_project = path
+            .unwrap()
+            .path()
+            .as_os_str()
+            .to_str()
+            .unwrap()
+            .to_string();
 
-        let path_project = path.unwrap().path().as_os_str().to_str().unwrap().to_string();
-        
-        //arguments for git command
-        let git_args = vec! ["-C",&path_project[..],"pull"];
-
-        let output = Command::new("git")
-            .args(git_args)
-            .output()
-            .expect("failed to execute process");
-
-        let hello = output.stdout;
-
-        let s = match str::from_utf8(&hello) {
-            Ok(v) => v,
-            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-        };
-
-        println!("{}", path_project);
-        println!("result: {}", s);
+        git_update_command(path_project);
     }
+}
+
+fn git_update_command(path_project: String) {
+    //arguments for git command
+    let git_args = vec!["-C", &path_project[..], "pull"];
+
+    let output = Command::new("git")
+        .args(git_args)
+        .output()
+        .expect("failed to execute process");
+
+    let hello = output.stdout;
+
+    let s = match str::from_utf8(&hello) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    println!("{}", path_project);
+    println!("result: {}", s);
 }
